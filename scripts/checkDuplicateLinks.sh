838dfdcb7ebd817e250cc4d4611b7f37ee5a1083
Loris Bettazza <loris.bettazza@gmail.com>
2019-09-28 21:51:10 +0200

    :art: Improve structure of checkDuplicateLinks

--- scripts/checkDuplicateLinks.sh
+++ scripts/checkDuplicateLinks.sh
@@ -3,11 +3,10 @@
 search_term="http"
 result=$(head -n 5 src/*/README.md | grep "$search_term" | cut -c18-63 | sort | uniq -c | grep -v "1 $search_term")
 
-echo "$result"
-
 if [ -z "$result" ]; then
   echo "[OK] No duplicate links found"
 else
-  echo "[ERR] Duplicate links found, every challenge sould have a unique link"
+  echo "[ERR] Duplicate links found, every challenge sould have a unique link\n"
+  echo "$result"
   exit 1
 fi
